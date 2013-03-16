46df5294df609a975019935b42f737611835b1b2
3cko <jmartin@ohaiworld.net>
2013-03-15 20:56:07 -0700

    created template

---
+++ CCV.java
@@ -0,0 +1,34 @@
+public class CCV
+{
+    public static void main(String[] args)
+    {
+    }
+
+    public static boolean isValid(long number)
+    {
+    }
+
+    public static int sumOfDoubleEvenPlace(long number)
+    {
+    }
+
+    public static int getDigit(int number)
+    {
+    }
+
+    public static int sumOfOddPlace(long number)
+    {
+    }
+
+    public static boolean prefixMatched(long number, int d)
+    {
+    }
+
+    public static int getSize(long d)
+    {
+    }
+
+    public static long getPrefix(long number, int k)
+    {
+    }
+}
