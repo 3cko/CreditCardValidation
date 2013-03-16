583f88076a70fea0146d7212e4ab1bb158bb721f
3cko <jmartin@ohaiworld.net>
2013-03-16 10:37:45 -0700

    added some pseudo code

--- CCV.java
+++ CCV.java
@@ -10,6 +10,16 @@ public class CCV
 
     public static int sumOfDoubleEvenPlace(long number)
     {
+        //total = 0
+        // for x in number.length()
+          // num = number.substring(x, x+1)
+          // num *= 2;
+          // if num.length() == 2
+            // a = num.substring(0,1)
+            // b = num.substring(1,2)
+            // num = a + b
+          // total += num
+
     }
 
     public static int getDigit(int number)
